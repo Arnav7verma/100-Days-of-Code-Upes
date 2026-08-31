4975b16a11ca40601ae2f058da0b917c05e27d85
Freelancer <arnavverma574@gmail.com>
2026-08-31 10:18:17 +0530

    Day 19:Loops without array/strings

---
+++ Day 19/question37.c
@@ -0,0 +1,35 @@
+//Q37: Write a program to find the LCM of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+4 5
+Output 1:
+20
+
+Input 2:
+7 3
+Output 2:
+21
+
+*/
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the 1st number");
+    scanf("%d",&a);
+    printf("Enter the 2nd number");
+    scanf("%d",&b);
+    int max=(a>b)? a:b;
+    while (1)
+    {
+        if (max%a==0 && max%b==0){
+            printf("%d",max);
+            break;
+        }
+        else
+            max++;
+    }
+    return 0;
+        
+}
---
+++ Day 19/question38.c
@@ -0,0 +1,27 @@
+//Q38: Write a program to find the sum of digits of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+123
+Output 1:
+6
+
+Input 2:
+999
+Output 2:
+27
+
+*/
+#include <stdio.h>
+int main(){
+    int n,sum=0;
+    printf("Enter the number");
+    scanf("%d",&n);
+    while (n>0)
+    {
+        sum=sum+n%10;
+        n=n/10;
+    }
+    printf("Sum:%d",sum);
+}
